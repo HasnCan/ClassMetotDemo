3b996d9c999d8207c89f79c999fb6326a0b5dd59
Hasan Can <hasan@DESKTOP-QK3E9F6>
2022-01-29 02:22:11 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Add(Musteri musteriler)
+        {
+            Console.WriteLine("Kişi Sisteme Kayıt edildi.");
+            Console.WriteLine("\nKişi İsmi: "+musteriler.FirstName);
+        }
+        public void Delete(Musteri musteriler)
+        {
+            Console.WriteLine("Kişinin Kaydı Sistemden Silinmiştir.");
+            Console.WriteLine("\nKişi İsmi: "+musteriler.FirstName);
+        }
+        public void ListofMusteri(Musteri[] musteriler)
+        {
+            Console.WriteLine("Kişiler Listelenmiştir.\n");
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine("Kişi İsmi: "+musteri.FirstName+"\nKişi Soyismi: "+musteri.LastName+"\nKişi Bakiyesi: "+musteri.Balance+"\n");
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.FirstName = "Behlül";
+            musteri1.LastName = "Zeki";
+            musteri1.Balance = 15468;
+
+            Musteri musteri2= new Musteri();
+            musteri2.Id = 2;
+            musteri2.FirstName = "Lale";
+            musteri2.LastName = "Akasya";
+            musteri2.Balance = 562;
+
+            Musteri musteri3=new Musteri();
+            musteri3.Id = 3;
+            musteri3.FirstName = "Ziya";
+            musteri3.LastName = "Yenilmez";
+            musteri3.Balance = 82;
+
+            Musteri[] musteriler=new Musteri[] { musteri1, musteri2, musteri3 };
+            MusteriManager musteriManager=new MusteriManager();
+
+            
+            musteriManager.Add(musteri1);
+            Console.WriteLine("-------------");
+            musteriManager.Delete(musteri3 );
+            Console.WriteLine("-------------");
+            musteriManager.ListofMusteri(musteriler);
+            Console.WriteLine("-------------");
+            
+
+
+        }
+    }
+}
